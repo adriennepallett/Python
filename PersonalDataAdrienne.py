abec8d0354fb3aaa0cee758510d07eb6acb5acde
AdrienneP-hoenix86 <adriennepallett@icloud.com>
2024-04-19 11:36:38 -0400

    Create PersonalDataAdrienne.py
    First..ish Python program. Print statements to display details about my education with programming

---
+++ PersonalDataAdrienne.py
@@ -0,0 +1,43 @@
+# File: apallettPersonalData
+# Project: CSIS2101 Assignment 1
+# Author: Adrienne Pallett
+# History: Version 1.2 August 29, 2022
+
+# 1) Name
+print ("Name: Adrienne Pallett")
+
+# 2) Email address
+print ("E-Mail Address: ag3358@mynsu.nova.edu")
+
+# 3) Major
+print ("Major: Information Technology")
+
+# 4) Status at Nova (full-time, part-time, non-degree seeking)
+print ("Status at Nova: Full-Time")
+
+# 5) Employment status (full-time, part-time, intern, student)
+print ("Employment Status: Student")
+
+# 6) Campus dorm or town where you now live
+print ("Campus Dorm / City of Residence: Plantation")
+
+# 7) Hometown where you grew up
+print ("Hometown: Plantation, FL")
+
+# 8) Previous programming course(s) taken, where, and instructor’s name
+print ("Previous Programming Course History:")
+print ("Art Institute of Dallas:")
+print (" - HTML, CSS : Professor A")
+print (" - Java : Professor Prince")
+print ("Nova Southeastern University:")
+print (" - TECH2020 : Python: Dr. Harward")
+print (" - CSIS2000 : SQL: Dr. Harward")
+
+# 9) Programming languages with which you have even a little experience
+print ("All Known Programming Languages: HTML, CSS, JavaScript, Python, SQL")
+
+#10) Favorite Movie or TV show you like to watch
+print ("Favorite TV Show: House of Dragon")
+
+#11) Favorite Hobby
+print ("Favorite Hobby: Traveling")
