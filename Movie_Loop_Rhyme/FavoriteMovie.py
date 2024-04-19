9e8302e64862e730c42b52b9c3ff73062d10c847
AdrienneP-hoenix86 <adriennepallett@icloud.com>
2024-04-19 12:08:11 -0400

    Create FavoriteMovie.py
    Takes in a favorite movie as an input and returns it as an output

---
+++ Movie_Loop_Rhyme/FavoriteMovie.py
@@ -0,0 +1,10 @@
+
+def Assignment5_Part1():
+    def Adrienne_fav_movie():
+            movie = str(input("What is your favorite movie?: "))
+            print ("My favorite movie is",movie)
+            print()
+        
+    Adrienne_fav_movie()
+
+Assignment5_Part1()
